dbdd9b746f887faa5196600daca403f50b9b8c8f
CLQuantizer <ezioxxk@gmail.com>
2022-12-24 23:52:46 +0000

    trying to understand shit

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,26 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let matches = App::new("ezio echor")
+        .version("0.1.0")
+        .author("Ezio <e@z.io>")
+        .about("Rust echo")
+        .arg(
+            Arg::with_name("ezio_text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+    
+    let text = matches.values_of_lossy("ezio_text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    print!("{}{}", text.join(" "), if omit_newline {""} else {"\n"});
 }
