0988dd6b97cef5f51393b9af9eab14e010ae0ec5
CLQuantizer <ezioxxk@gmail.com>
2022-12-27 19:52:56 +0000

    use can go inside function

--- iter_demo/src/main.rs
+++ iter_demo/src/main.rs
@@ -1,19 +1,33 @@
-fn main() {
-    comparison();
-    let needle = 0xCB;
-    let haystack = [1,1,2,5,15,52,203,877, 4140,21147];
-    for item in haystack.iter(){
-        if *item == needle {
+fn comparison() {
+    let needle = 0xcb;
+    let haystack = [1, 1, 2, 5, 15, 52, 203, 877, 4140, 21147];
+    for item in haystack.into_iter() {
+        // we do not need deref here
+        if item == needle {
             println!("{}", item);
         }
     }
 }
-fn comparison(){
-    let needle = 0xCB;
-    let haystack = [1,1,2,5,15,52,203,877, 4140,21147];
-    for item in haystack.into_iter(){
-        // we do not need deref here
-        if item == needle {
+
+fn time_demo() {
+    use std::time::{Duration, Instant};
+    let mut count = 0;
+    let time_limit = Duration::new(1, 0);
+    let start = Instant::now();
+
+    while Instant::now() - start < time_limit {
+        count += 1;
+    }
+    println!("{}", count);
+}
+
+fn main() {
+    time_demo();
+    comparison();
+    let needle = 0xcb;
+    let haystack = [1, 1, 2, 5, 15, 52, 203, 877, 4140, 21147];
+    for item in haystack.iter() {
+        if *item == needle {
             println!("{}", item);
         }
     }
