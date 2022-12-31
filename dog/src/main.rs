bf6ec30360dbeab74b57f2c2a5dccc79e47bf94f
CLQuantizer <ezioxxk@gmail.com>
2022-12-31 00:00:55 +0000

    the integer system of Rust is too dope

--- dog/src/main.rs
+++ dog/src/main.rs
@@ -1,4 +1,5 @@
 use std::ops::Add;
+use std::ops::Sub;
 
 struct Dog {name: String,age: u32,}
 
@@ -9,10 +10,17 @@ impl Add for Dog {
     fn add(self, other: Dog) -> u32 {self.age + other.age}
 }
 
+impl Sub for Dog{
+    type Output = u32;
+    fn sub(self, other: Dog) -> u32 {self.age - other.age}
+}
+
 fn main() {
     let dog1 = Dog {name: "Rufus".to_string(),age: 5};
     let dog2 = Dog {name: "Rusty".to_string(),age: 7,};
     // 這裡有 operator overload
     let total_age = dog1 + dog2;
+    // let age_diff = dog1 - dog2;
     println!("The total age is {}", total_age);
+    // println!("The age diff is {}", age_diff);
 }
