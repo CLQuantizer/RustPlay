9fdf9943cee548c42c6abc52cd1363e4a1d1bd2f
Ez <ezioxxk@gmail.com>
2023-01-02 17:23:18 +0000

    add test shit

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use assert_cmd::Command;
 use predicates::prelude::*;
 
@@ -12,8 +14,19 @@ fn dies_no_args(){
 
 #[test]
 fn runs(){
-    //
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("hello").assert().success();
     cmd.arg("damn").assert().success();
+}
+
+#[test]
+fn compare(){
+    let outfile = "test/expected/hello1.n.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    //be careful there is no \n at the end of hello1.n.txt
+    cmd
+        .arg("-n")
+        .arg("Hello  there").assert().success().stdout(expected);
+
 }
