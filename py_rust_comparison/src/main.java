40815db87e88457346c59857c681a9da6043af18
Ez <ezioxxk@gmail.com>
2023-01-11 21:28:40 +0000

    java

---
+++ py_rust_comparison/src/main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Dog {
+    int id;
+
+    Dog(int id) {
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        Stack<Dog> l = new Stack<>();
+        l.add(new Dog(1));
+        l.add(new Dog(2));
+        l.add(new Dog(3));
+        Dog dog3 = l.peek();
+        l.peek();
+        System.out.println(dog3.id);
+    }
+}
