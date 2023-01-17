ccaeabbe3dee4e8b82a3615dc84a3db00e1b2ea2
Ez <ezioxxk@gmail.com>
2023-01-17 22:19:54 +0000

    closure

---
+++ closure_demo/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let num: u8 = 127;
+    let print_num = |name: &str, x:u8|{
+        println!("{}: {}",name, x);
+    }
+    let print_res = |adder: u8|{
+        print_num("adder", adder);
+        print_num("num", num);
+        println!("num+adder={}",adder+num);
+    };
+    print_res(8);
+}
