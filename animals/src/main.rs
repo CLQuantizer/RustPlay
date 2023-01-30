64f30b0c569c24ee19df45b502febee7ef753c34
Ez <ezioxxk@gmail.com>
2023-01-30 01:03:05 +0000

    the std::any::type_name thing is amazing

--- animals/src/main.rs
+++ animals/src/main.rs
@@ -1,7 +1,9 @@
-use animals::{Cat, Dog};
+use animals::{Cat, Dog, print_type_of};
 fn main() {
     let cat = Cat::new("Mittens");
     let dog = Dog::new("Fido");
-    println!("Cat: {}", cat.name);
+    print_type_of(&cat);
     println!("Dog: {}", dog.name);
+    print_type_of(&print_type_of::<i32>); // playground::print_type_of<i32>
+    print_type_of(&{ || "Hi!" }); // playground::main::{{closure}}
 }
